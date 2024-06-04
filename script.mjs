0180343e2964ab174d2924c7b143d1197ebd3f3c
Kittypoda <odakrolstad@gmail.com>
2024-06-04 12:49:20 +0200

    slide button ok

---
+++ script.mjs
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const sliderCheckbox = document.getElementById('slider');
+
+  sliderCheckbox.addEventListener('change', function () {
+    if (this.checked) {
+      window.location.href = '/html/work.html';
+    } else {
+      window.location.href = '/index.html';
+    }
+  });
+});
+
