a09699f0e74141c161df5d8701b16f3ae3821c67
Kittypoda <odakrolstad@gmail.com>
2025-06-06 17:47:56 +0200

    bio update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,12 +16,11 @@ function About({ language }) {
           <>
             <p className="mb-4">
             Hei! Jeg heter Oda og jobber i skjæringspunktet mellom grafisk design og front-end-utvikling. <br />
-    Jeg bor i et lite rosa hus ved en norsk fjord – omgitt av skog, blomster og masse kreativt driv.
+            Jeg bor i et lite rosa hus ved en norsk fjord – omgitt av skog, blomster og masse kreativt driv.
   </p>
           <p className="mb-4">
             Jeg liker å utvikle visuelle opplevelser som både <em>føles</em> riktige og <em>fungerer</em> godt.  
-            Med interesse for estetikk og detaljer forsøker jeg å lage løsninger som er rene, inspirerende og enkle å bruke – gjerne med et snev av lekenhet eller personlighet.  
-            Jeg er fortsatt i utvikling, men elsker å jobbe i det spenningsfeltet der design møter funksjon.
+            Med interesse for estetikk og detaljer forsøker jeg å lage løsninger som er rene, inspirerende og enkle å bruke – helst med et snev av lekenhet og personlighet.  
           </p>
           
             <p>
@@ -36,8 +35,7 @@ function About({ language }) {
             </p>
             <p className="mb-4">
               I enjoy creating visual experiences that both <em>feel</em> right and <em>function</em> well.  
-              With an interest in aesthetics, and detail, I aim to build solutions that are clean, inspiring, and easy to use – often with a touch of playfulness or personality.  
-              I’m still learning and growing, but I love working in the space where design meets functionality.
+              With an interest in aesthetics, and detail, I aim to build solutions that are clean, inspiring, and easy to use – Preferably with a detail that adds a bit of edge or surprise.  
             </p>
             <p>
               Feel free to reach out at{" "}
