5fbfcf29368b4d6d0a9e99f8b57161d0b8acaeba
Kittypoda <odakrolstad@gmail.com>
2025-08-14 18:44:25 +0200

    loader updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,10 @@
-import odaImage from '../assets/images/oda.png';
+import odaodaImage from '../assets/images/groupoda.png';
 
 function Loader() {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#DFB6D9]">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-(14deg, #F392A5 1.18%, #A72225 100.81%)">
       <img
-        src={odaImage}
+        src={odaodaImage}
         alt="Oda logo spinner"
         className="w-42 h-42 animate-spin-slow opacity-80"
       />
